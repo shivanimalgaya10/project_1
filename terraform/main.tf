8db7deb7df183b0fe229823b11606525c6201b8a
shivani malgaya <shivanimalgaya10@gmail.com>
2026-02-22 18:20:46 +0530

    tenth  commit9

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ data "aws_subnets" "default" {
 }
 
 resource "aws_security_group" "devops_sg" {
-    name   = "devops-sg34"
+    name   = "devops-sg345"
     vpc_id = data.aws_vpc.default.id
 
     ingress {
